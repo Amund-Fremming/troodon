4d5b23f3d7a252c84b1cb77f44bf54d4f69faa6c
amund-fremming <amund.fremming@gmail.com>
2024-09-15 21:07:32 +0200

    Added a todo

---
+++ TODO.md
@@ -0,0 +1,18 @@
+# TODO
+
+- [ ] Set up functions for getting input from command line
+- [ ] Set up functions for parsing input from command line
+- [ ] Set up functions for creating files in file tree
+- [ ] Set up functions for creating folders in file tree
+- [ ] Create the Class engine for holding the data and doing th ebusiness of the program
+- [ ] Create a instanse of the Class engine and run it
+
+## UI
+
+- Use color red to mark out important names or feedback
+- When finished output the files and folders tree structure
+- Use Options to ask if the user wants OAuth 2.0
+
+## Features
+
+- Add commands for adding more models after build
--- obj/Debug/net8.0/troodon.AssemblyInfo.cs
+++ obj/Debug/net8.0/troodon.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("troodon")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ebb0130a45f3112a29a5b76f17781d53c2e70c1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+88527e2e6ab9dd9edfcb92e79aecd4eb8d92a58e")]
 [assembly: System.Reflection.AssemblyProductAttribute("troodon")]
 [assembly: System.Reflection.AssemblyTitleAttribute("troodon")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
